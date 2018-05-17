29d711c7ec2f34a50df9f5eb5a8e1a028ad1a7c2
wangweilucky <605479355@qq.com>
2018-05-17 16:07:18 +0800

    update：删除我们所有Codable协议实现的代码。 只留下属性的声明。因为Codable默认帮我们实现了Encoding和Decoding的实现

--- encoding_decoding/Plane.swift
+++ encoding_decoding/Plane.swift
@@ -14,27 +14,6 @@ struct PlaneResult: Decodable, Encodable {
     var message: String
     var code: Int
     
-    
-    /// encoder和decoder的实现
-    private enum CodingKeys: String, CodingKey {
-        case data
-        case code
-        case message
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.data = try container.decode(Plane.self, forKey: .data)
-        self.message = try container.decode(String.self, forKey: .message)
-        self.code = try container.decode(Int.self, forKey: .code)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.message, forKey: .message)
-        try container.encode(self.data, forKey: .data)
-        try container.encode(self.code, forKey: .code)
-    }
 }
 
 
@@ -42,29 +21,6 @@ struct Plane: Decodable , Encodable {
     var manufacturer: String
     var model: String
     var seats: Int
-    
-    
-    /// encoder和decoder的实现
-    private enum CodingKeys: String, CodingKey {
-        case manufacturer
-        case model
-        case seats
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.manufacturer = try container.decode(String.self, forKey: .manufacturer)
-        self.model = try container.decode(String.self, forKey: .model)
-        self.seats = try container.decode(Int.self, forKey: .seats)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.manufacturer, forKey: .manufacturer)
-        try container.encode(self.model, forKey: .model)
-        try container.encode(self.seats, forKey: .seats)
-    }
-    
     
 }
 
